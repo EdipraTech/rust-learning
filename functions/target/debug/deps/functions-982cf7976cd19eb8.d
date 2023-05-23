5ace0b470b52a54a6e14763d3739c00375eb25aa
Super User <root@Daniel-PC>
2023-05-23 18:54:23 -0400

    functions

---
+++ functions/target/debug/deps/functions-982cf7976cd19eb8.d
@@ -0,0 +1,5 @@
+/mnt/c/Programming/Rust/Learning/functions/target/debug/deps/functions-982cf7976cd19eb8: src/main.rs
+
+/mnt/c/Programming/Rust/Learning/functions/target/debug/deps/functions-982cf7976cd19eb8.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/mnt/c/Programming/Rust/Learning/functions/target/debug/functions: /mnt/c/Programming/Rust/Learning/functions/src/main.rs
