c3695f8bb29e473b656eafbd8f5f6ee1a3caaa78
Daniel Arpide <edipratech@gmail.com>
2023-05-24 14:57:14 -0400

    slices

---
+++ slices/target/debug/deps/slices-313bc419a36032cb.d
@@ -0,0 +1,5 @@
+/home/edipratech/Programming/Rust/Learning/slices/target/debug/deps/slices-313bc419a36032cb: src/main.rs
+
+/home/edipratech/Programming/Rust/Learning/slices/target/debug/deps/slices-313bc419a36032cb.d: src/main.rs
+
+src/main.rs:
---
+++ slices/target/debug/slices.d
@@ -0,0 +1,1 @@
+/home/edipratech/Programming/Rust/Learning/slices/target/debug/slices: /home/edipratech/Programming/Rust/Learning/slices/src/main.rs
